9c795c12e85a2158fd33b2c0217fda7f8d67de22
Leo <braian.tamay@hotmail.com.ar>
2019-11-13 20:57:11 -0300

    login

--- proyectofinalback/app/Http/Controllers/UserController.php
+++ proyectofinalback/app/Http/Controllers/UserController.php
@@ -9,6 +9,17 @@ use Validator;
 
 class UserController extends Controller
 {
+    public function login()
+    {
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])) {//validar que el usuario existe en la bd 
+            $user = Auth::user();//obtenemos el usuario logueado
+            $success['token'] =  $user->createToken('MyApp')->accessToken; //creamos el token
+            return response()->json(['success' => $success], 200);//se lo enviamos al usuario
+        } else {
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        }
+    }
+    
     public function register(Request $request)
     { 
         $validator = Validator::make($request->all(), [ //creamos la validación
